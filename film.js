4b812de81d6841862067a88ecb2b41029f053d8b
vinny2001 <vincenzo.daria01@gmail.com>
2024-06-25 12:11:08 -0400

    Film Page (So far)

--- film.js
+++ film.js
@@ -9,13 +9,8 @@ console.log("Id: " + id);
 async function fetchFilms() {
     try {
         const film = await fetch(`https://swapi2.azurewebsites.net/api/films/${id}`);
-        const character = await fetch(`https://swapi2.azurewebsites.net/api/films/${id}/characters`);
-        const planet = await fetch(`https://swapi2.azurewebsites.net/api/films/${id}/planets`);
         const filmResponse = await film.json();
-        const characterResponse = await character.json();
-        const planetResponse = await planet.json();
-        
-        displayFilmInfo(filmResponse, characterResponse, planetResponse);
+        displayFilmInfo(filmResponse);
         if (!film.ok) {
             throw new Error("Network response was not OK");
         }
@@ -26,7 +21,27 @@ async function fetchFilms() {
 }
 fetchFilms();
 
-function displayFilmInfo(film, character, planet){
+// Fetch Characters
+async function fetchCharacters(id) {
+    let characterUrl = `https://swapi2.azurewebsites.net/api/films/${id}/characters`;
+    const characters = await fetch(characterUrl)
+    const response = await characters.json();
+    displayCharacters(response);
+    console.log("Characters: " + response);
+}
+fetchCharacters(id);
+
+// Fetch Planets
+async function fetchPlanets(id) {
+    let planetURL = `https://swapi2.azurewebsites.net/api/films/${id}/planets`;
+    const planets = await fetch(planetURL)
+    const response = await planets.json();
+    displayPlanets(response);
+    console.log("Planets: " + response);
+}
+fetchPlanets(id);
+
+function displayFilmInfo(film){
     
     let filmHeader = document.createElement('tr');
     filmHeader.innerHTML = `
@@ -35,9 +50,8 @@ function displayFilmInfo(film, character, planet){
 		<th scope="col">Episode</th>
 		<th scope="col">Director</th>
 		<th scope="col">Release Date</th>
-		<th scope="col">Opening Crawl</th>
+		
 	`;
-
     
     let filmDiv = document.createElement('tr');
     filmDiv.innerHTML =`
@@ -46,10 +60,32 @@ function displayFilmInfo(film, character, planet){
         <td>${film.episode_id}</td>
         <td>${film.director}</td>
         <td>${film.release_date}</td>
-        <td>${film.opening_crawl}</td>
     `;
+    
     //Append table body to DOM
     document.getElementById('table-header').appendChild(filmHeader);
     document.getElementById('populateBody').appendChild(filmDiv);
     
+}
+
+function displayPlanets(planet){
+    let planetDiv = document.createElement('ul');
+    for(const key in planet){
+        planetDiv.innerHTML =`
+            <li>${planet[key].name}</li>
+
+        `;
+    }
+    document.getElementById('planetList').appendChild(planetDiv);
+}
+
+function displayCharacters(char){
+    let charDiv = document.createElement('ul');
+    
+    for(const key in char){
+        charDiv.innerHTML =`
+            <li>${char[key].name}</li>
+        `;
+    }
+    document.getElementById('characterList').appendChild(charDiv);
 }
