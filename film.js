cc7d357baf371af2e27677dafddb47323de28017
vinny2001 <vincenzo.daria01@gmail.com>
2024-06-25 14:49:28 -0400

    Fix Films Screen

--- film.js
+++ film.js
@@ -1,39 +1,33 @@
 
-//Get the id
-const sp = new URLSearchParams(window.location.search);
-const id = sp.get('id');
-console.log("Id: " + id);
 
+//NEW
+let producerSpan;
+let planetsUl;
+let characterUl;
+const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
+
+// Runs on page load
 addEventListener('DOMContentLoaded', () => {
-    nameH1 = document.querySelector('h1#planet_name');
-    climateSpan = document.querySelector('span#climate');
-    diameterSpan = document.querySelector('span#diameter');
-    gravitySpan = document.querySelector('span#gravity');
-    orbital_periodSpan = document.querySelector('span#orbital_period');
-    populationSpan = document.querySelector('span#population');
-    rotation_periodSpan = document.querySelector('span#rotation_period');
-    surface_waterSpan = document.querySelector('span#surface_water');
-    terrainSpan = document.querySelector('span#terrain');
-    planetsUl = document.querySelector('#planets>ul');
-    charactersUl = document.querySelector('#characters>ul');
-    const sp = new URLSearchParams(window.location.search)
-    const id = sp.get('id')
-    
+  nameH1 = document.querySelector('h1#name');
+  producerSpan = document.querySelector('span#producerSpan');
+  planetsUl = document.querySelector('#planets>ul');
+  characterUl = document.querySelector('#characters>ul');
+  const sp = new URLSearchParams(window.location.search)
+  const id = sp.get('id')
+  fetchFilms(id)
 });
 
+
 //Fetch Film from API (GET request)
-async function fetchFilms() {
+async function fetchFilms(id) {
     let film;
-    try {
-        film = await fetch(`https://swapi2.azurewebsites.net/api/films/${id}`);
-        film.planets = await fetchPlanets(film);
-        film.characters = await fetchCharacters(film);
-        const filmResponse = await film.json();
-        displayFilmInfo(filmResponse);
-        if (!film.ok) {
-            throw new Error("Network response was not OK");
-        }
-        
+    try{
+      film = await fetchFilm(id)
+      console.log(film);
+      film.planets = await fetchPlanets(film);
+      film.characters = await fetchCharacters(film);
+      
     } catch (error) {
       console.error("There was a problem with your fetch request: ", error);
     }
@@ -41,46 +35,11 @@ async function fetchFilms() {
 }
 fetchFilms();
 
-function displayFilmInfo(film){
-    
-    let filmHeader = document.createElement('tr');
-    filmHeader.innerHTML = `
-		<th scope="col">Producer</th>
-		<th scope="col">Title</th>
-		<th scope="col">Episode</th>
-		<th scope="col">Director</th>
-		<th scope="col">Release Date</th>
-		
-	`;
-    
-    let filmDiv = document.createElement('tr');
-    filmDiv.innerHTML =`
-        <td>${film.producer}</td>
-        <td>${film.title}</td>
-        <td>${film.episode_id}</td>
-        <td>${film.director}</td>
-        <td>${film.release_date}</td>
-    `;
-    
-    //Append table body to DOM
-    document.getElementById('table-header').appendChild(filmHeader);
-    document.getElementById('populateBody').appendChild(filmDiv);
-    
-}
-
-
-//NEW
-
-let planetsUl;
-let charactersUl;
-const baseUrl = `https://swapi2.azurewebsites.net/api`;
-
-
-/* async function fetchFilm(id) {
+async function fetchFilm(id) {
   let filmUrl = `${baseUrl}/films/${id}`;
   return await fetch(filmUrl)
     .then(res => res.json())
-} */
+}
 
 async function fetchCharacters(film) {
   const url = `${baseUrl}/films/${film?.id}/characters`;
@@ -97,9 +56,12 @@ async function fetchPlanets(film) {
 }
 
 const renderFilms = film => {
-    
-    const planetsLis = film?.planets?.map(planet => `<li><a href="/film.html?id=${planet.id}">${planet.name}</li>`)
-    planetsUl.innerHTML = planetsLis.join("");
-    const charactersLis = film?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
-    charactersUl.innerHTML = charactersLis.join("");
+  document.title = `SWAPI - ${film?.title}`;  // Just to make the browser tab say their name
+  nameH1.textContent = film?.title;
+  producerSpan.textContent = film?.producer;
+  const planetsLis = film?.planets?.map(planet => `<li><a href="/planet.html?id=${planet.id}">${planet.name}</li>`)
+  planetsUl.innerHTML = planetsLis.join("");
+  const charactersLis = film?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  characterUl.innerHTML = charactersLis.join("");
+  console.log("RENDERFILMS")
 }
